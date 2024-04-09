d273649bd30667d6ab232f45e1371dc88a330f32
Daniel Salvatore <dsalvatore105@outlook.com>
2024-04-09 16:04:24 -0400

    Counter testing implemented

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,32 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import {render, screen, fireEvent} from '@testing-library/react' // import necessary react testing library helpers here
+import Counter from '../components/Counter' // import the Counter component here
 
 beforeEach(() => {
-  // Render the Counter component here
+  render(Counter)
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const counter_message = screen.getByText('Counter');
+  expect(counter_message).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  const count = screen.getByTestId('count')
+  expect(count).toHaveTextContent('0');
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const increment_button = screen.getByText('+');
+  const count = screen.getByTestId('count')
+
+  fireEvent.click(increment_button);
+  expect(count).toHaveTextContent('1')
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const decrement_button = screen.getByText('-');
+  const count = screen.getByTestId('count')
+
+  fireEvent.click(decrement_button);
+  expect(count).toHaveTextContent('-1')
 });
